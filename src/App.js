b6902d8a5bd1947e7b0cda9d776738179a6afa37
reza-bd <mdselimreza.se.bd@gmail.com>
2022-12-26 02:01:32 +0600

    extra removal

--- src/App.js
+++ src/App.js
@@ -1,20 +1,9 @@
-import { Route, Routes } from 'react-router-dom';
 import './App.css';
-import Product from './components/Body/Product/Product';
-import Header from './components/Header/Header';
-import Footer from './components/Footer/footer';
+
 function App() {
   return (
     <div className="App">
-      <Header></Header>
-      <Routes>
-        <Route path='/' element={<Home></Home>}></Route>
-        <Route path='/' element={<Product></Product>}></Route>
-        <Route path='/' element={<Order></Order>}></Route>
-        <Route path='/' element={<Login></Login>}></Route>
-        <Route path='/' element={<Registration></Registration>}></Route>
-      </Routes>
-      <Footer></Footer>
+      
     </div>
   );
 }
